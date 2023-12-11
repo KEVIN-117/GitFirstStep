368a14f9906c6142097f2ef5ddebcd549e06b788
Kevin Alexis Rodriguez Condori <sis414.kevinrodriguez17@gmail.com>
2023-12-11 00:26:43 -0400

    navbar

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About(){
+  return (
+    <div className='text-center'>
+      <h1 className='text-indigo-600 text-4xl'>About</h1>
+      <p className='text-white text-sm'>This is the about page</p>
+    </div>
+  )
+}
